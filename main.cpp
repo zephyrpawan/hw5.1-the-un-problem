a679d6b6480101a54d4dc500a7da266a0da43f94
zephyrpawan <zephyrpawan@gmail.com>
2022-06-13 03:25:28 -0500

    code cleanup

--- main.cpp
+++ main.cpp
@@ -15,14 +15,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    // Initialize a ofstream to outputfile [optional]
+    // Initialize ofstream to outputfile [optional] for logging
     // For testing and debugging purposes details will be printed to output.txt
     ofstream outputData;
     outputData.open("output.txt");
     if (!outputData)
     {
         cerr << "Unable to open file!" << endl;
-        exit(-1);
     }
 
     // Initialize a member vector to hold all the members from each line in the input file
@@ -31,10 +30,10 @@ int main(int argc, char const *argv[])
     // Read inputfile to populate memberVector and set number of members
     int numberOfMembers = Utilities::readInputFile(&memberVector, "input.txt");
 
-    // Check to make sure first line of input file contains correct number of members
+    // Check to make sure first line of inputfile contains correct number of members before proceeding
     Utilities::exitIfBadInputFile(numberOfMembers, memberVector.size());
 
-    // Print member count and the list of members to outputfile.txt
+    // Print member count and the list of members to outputfile.txt [optional]
     outputData << "Number of UN Members in a meeting = " << numberOfMembers << endl;
     Utilities::printMemberVector(memberVector, outputData);
     outputData << endl;
@@ -50,9 +49,10 @@ int main(int argc, char const *argv[])
     // Each vector in stronglyConnectedMembersVec contains members who can converse with each other
     vector<vector<Member>> stronglyConnectedMembersVec = UNMemberGraph.findSCC();
 
+    // write to oputput file [optional]
     outputData << endl << "List of memers who can all converse with each other: " << endl;
 
-    // Initialize the size of strongly connected members
+    // Initialize the  max size of strongly connected members
     int maxSccMembersSize = 0;
 
     // loop through stronglyConnectedMembers to find the largest group size that can converse
@@ -62,21 +62,25 @@ int main(int argc, char const *argv[])
         {
             maxSccMembersSize = stronglyConnectedMembers.size();
         }
-        // Print an edge member1-->member2 as "member2 understands member1" to the file output.txt
+        // Print an edge m1-->m2 as "m1 speaks *language* and m2 understands m1" to the file output.txt [optional]
         Utilities::printMemberVector(stronglyConnectedMembers, outputData);
         
     }
 
+    // printing to outputfile [optional]
     outputData << endl;
     outputData << "Maximum number of members who can all converse with each other: " << maxSccMembersSize << endl;
     outputData << endl;
 
+    // Calculate the number of members required to leave
     int membersRequiredToLeave = numberOfMembers - maxSccMembersSize;
 
+    // Print the output to the terminal and the file
     outputData << "Minimum number of members required to leave = " << membersRequiredToLeave;
     cout << membersRequiredToLeave;
     cout << endl;
 
+    // closing ofsream
     outputData.close();
 
     return 0;
